67f53ade3b0211c65e644329e3fe1de1413fc98e
vignesh44444 <118652563+vignesh44444@users.noreply.github.com>
2022-11-20 12:17:01 +0530

    Add files via upload

---
+++ vignesh python.py
@@ -0,0 +1,21 @@
+print("1")
+Username = input ("Enter your Username:")
+Password = input ("Enter your Password:")
+if (Username == "vignesh" and Password == "luffyzoro"):
+   print ("welcome vignesh")
+else:
+   print ( "wrong credentials")
+
+
+print("2")
+number=int(input("enter the value to check whether it is odd or even"))
+if(number%2)==0:
+   print ("It is even number")
+else:
+   print("It is odd number")
+
+
+print("3")
+fruits = ["Blueberry","Apricot","Peach","Grapefruit","Kiwi"]
+for fruit in fruits:
+  print(fruit)
